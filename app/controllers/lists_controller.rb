fb81a1a0f6a521b8ce5c8682659ba6c3a7abb486
jigangdade60 <255278098+jigangdade60@users.noreply.github.com>
2026-02-06 14:40:49 +0000

    [Finish] 3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,7 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list.id)
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
 
   end
